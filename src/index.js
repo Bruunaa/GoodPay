4d75d8f8df6d5e638ead90347b937d9b3a83c5eb
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-04-20 13:37:08 -0300

    Banner com textos atualizados e Carrossel.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
 
 function rotaCadastro() {
   window.location.href = "../registro.html";
@@ -9,9 +10,19 @@ function rotaRecuperar() {
 function rotaLogin() {
   let email = document.getElementById("email").value;
   let senha = document.getElementById("senha").value;
-  if (email === "bruunaaa@gmail.com" && senha === "00123") {
+  if (email === "bruunaa@gmail.com" && senha === "00123") {
     window.location.href = "../index.html";
   } else {
     janelaLogin.show();
   }
+}
+
+function alertaPromo(){
+  let titulo ="Promomção 1 [Quebra da Banca]";
+  let corpo = "<h6>Super legal</h6> <br> <img src='./img/4.png' width='160' height='160'>";
+  
+  janelaPromocao.show();
+
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("corpo").innerHTML = corpo; 
 }
